6dc4c809acd54629a14bb155be3ac0a6ac85f402
rakesh <rakeshreddy@gmail.com>
2026-03-01 22:05:03 +0530

    modified main.tf

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo yum install ansible -y
-ansible-pull -U https://github.com/rakesh6658/ansible-roboshop-roles-tf.git -e component="mongodb" main.yaml
+ansible-pull -U https://github.com/rakesh6658/ansible-roboshop-roles-tf.git -i inventory.ini -e component="mongodb" main.yaml
 
