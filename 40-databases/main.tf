3311f95f07ff7bd46beea38078cc137be8a443d8
rakesh <rakeshreddy@gmail.com>
2026-03-04 14:42:31 +0530

    added route53

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -150,3 +150,37 @@ resource "terraform_data" "mysql" {
      ]
   }
 }
+resource "aws_route53_record" "mongodb" {
+  zone_id = var.zone_id
+  name    = "mongodb-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mongodb.private_ip]
+  depends_on = [ aws_instance.mongodb ]
+}
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.redis.private_ip]
+  depends_on = [ aws_instance.redis ]
+}
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.rabbitmq.private_ip]
+  depends_on = [ aws_instance.rabbitmq ]
+}
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mysql.private_ip]
+  depends_on = [ aws_instance.mysql ]
+}
+
+
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -6,3 +6,11 @@ variable "environment" {
     default = "dev"
   
 }
+variable "zone_id" {
+    default = "Z01170992B3XLBKODT7HI"
+  
+}
+variable "domain_name" {
+    default = "joindevops.online"
+  
+}
