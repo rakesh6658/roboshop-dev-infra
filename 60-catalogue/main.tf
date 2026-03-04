997086d7b0f8437f9d323f42ffcc225afe2d1477
rakesh <rakeshreddy@gmail.com>
2026-03-04 15:23:12 +0530

    modified catalogue

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -33,13 +33,13 @@ resource "terraform_data" "catalogue" {
   }
   
 }
-resource "aws_ec2_instance_state" "catalogue" {
-  instance_id = aws_instance.catalogue.id
-  state       = "stopped"
-  depends_on = [ terraform_data.catalogue ]
-}
-resource "aws_ami_from_instance" "catalogue" {
-  name               = "catalogue-ami"
-  source_instance_id = aws_instance.catalogue.id
-  depends_on = [ aws_ec2_instance_state.catalogue ]
-}
+# resource "aws_ec2_instance_state" "catalogue" {
+#   instance_id = aws_instance.catalogue.id
+#   state       = "stopped"
+#   depends_on = [ terraform_data.catalogue ]
+# }
+# resource "aws_ami_from_instance" "catalogue" {
+#   name               = "catalogue-ami"
+#   source_instance_id = aws_instance.catalogue.id
+#   depends_on = [ aws_ec2_instance_state.catalogue ]
+# }
